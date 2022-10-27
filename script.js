2cc56b70c9092a13edee9503d04e533c26bd4bb0
Kaleb Sarten <kaleb@Kalebs-MacBook-Pro.local>
2022-10-27 14:05:59 +0900

    Updated variable names for increased clarity

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 //A factory function for generating workout objects
-const workoutGenerator = (dayName, majorLifts, minorLifts) => {
+const workoutGenerator = (muscleGroup, majorLifts, minorLifts) => {
     return {
-        dayName: dayName,
+        muscleGroup: muscleGroup,
         majorLifts: majorLifts,
         minorLifts: minorLifts,
         getRandomMajor() {
-            return this.majorLifts[Math.floor(Math.random() * majorLifts.length)]
+            return this.majorLifts[Math.floor(Math.random() * this.majorLifts.length)]
         },
         getRandomMinor() {
-            return this.minorLifts[Math.floor(Math.random() * minorLifts.length)]
+            return this.minorLifts[Math.floor(Math.random() * this.minorLifts.length)]
         }
     }
 }
@@ -41,7 +41,7 @@ const daysOfProgram = {
 }
 
 //Store the set/rep protocols in variables for easier interpolation later
-const reps = {
+const repScheme = {
     1: '3 sets of 5 reps',
     2: '4 sets of 8 reps',
     3: '3 sets of 12 reps'
@@ -51,13 +51,13 @@ const reps = {
 let day = daysOfTheWeek[Math.ceil(Math.random() * 7)];
 
 //Create workout variable for easier access later.
-let workout = daysOfProgram[day];
+let todaysWorkout = daysOfProgram[day];
 
 //Create empty objects as containers for the randomized workouts that will occur on that day.
-let majors = {
+let majorLifts = {
 
 }
-let minors = {
+let minorLifts = {
     
 }
 
@@ -66,17 +66,17 @@ console.log(`\nGood Morning! It's ${day}!`);
 
 //If rest day, print rest message.
 //If a workout day, generate random major and minor exercises, ensuring no duplicates, then print message using interpolation.
-if (workout === 'Rest') {
+if (todaysWorkout === 'Rest') {
     console.log('Today is a rest day. Take the day off, and try not to work too hard. Let your body rebuild and come back stronger!\n\n');
 } else {
-    majors[1] = workout.getRandomMajor();
+    majorLifts[1] = todaysWorkout.getRandomMajor();
     do {
-        majors[2] = workout.getRandomMajor();
-    } while (majors[1] === majors[2]);
+        majorLifts[2] = todaysWorkout.getRandomMajor();
+    } while (majorLifts[1] === majorLifts[2]);
 
-    minors[1] = workout.getRandomMinor();
+    minorLifts[1] = todaysWorkout.getRandomMinor();
     do {
-        minors[2] = workout.getRandomMinor();
-    } while (minors[1] === minors[2]);
-    console.log(`Today is ${workout.dayName} day! Today you will perform:\n\n\t${reps[1]} of ${majors[1]}\n\t${reps[2]} of ${majors[2]}\n\t${reps[3]} of ${minors[1]}\n\t${reps[3]} of ${minors[2]}\n\n`);
+        minorLifts[2] = todaysWorkout.getRandomMinor();
+    } while (minorLifts[1] === minorLifts[2]);
+    console.log(`Today is ${todaysWorkout.muscleGroup} day! Today you will perform:\n\n\t${repScheme[1]} of ${majorLifts[1]}\n\t${repScheme[2]} of ${majorLifts[2]}\n\t${repScheme[3]} of ${minorLifts[1]}\n\t${repScheme[3]} of ${minorLifts[2]}\n\n`);
 };
